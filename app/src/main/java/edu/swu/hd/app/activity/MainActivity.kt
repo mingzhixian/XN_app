3723096cbf1e1adcac147452578299418582b57b
mingzhixian <mingzhixian123@gmail.com>
2023-06-14 14:38:32 +0800

    修改工具库

--- app/src/main/java/edu/swu/hd/app/activity/MainActivity.kt
+++ app/src/main/java/edu/swu/hd/app/activity/MainActivity.kt
@@ -8,17 +8,34 @@ import androidx.lifecycle.ViewModelStoreOwner
 import edu.swu.hd.app.R
 import edu.swu.hd.app.helper.AppData
 
+lateinit var appData: AppData
+
 class MainActivity : Activity(), ViewModelStoreOwner {
     companion object {
         var ViewModelStore: ViewModelStore? = null
     }
 
-    // 数据
-    val appData = ViewModelProvider(this).get(AppData::class.java)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        appData = ViewModelProvider(this).get(AppData::class.java)
+        // 第一次使用
+        if (appData.settings.getBoolean("FirstUse", true)) firstUse()
+        // 检测登录
+        
+    }
+
+    // 第一次使用
+    private fun firstUse() {
+        // 通知注册
+        appData.notificationHelper.registerChannel()
+        // 权限申请
+        appData.notificationHelper.requireNotificationPermission()
+        // 保存
+        appData.settings.edit().apply {
+            putBoolean("FirstUse", false)
+            apply()
+        }
     }
 
     override fun getViewModelStore(): ViewModelStore {
