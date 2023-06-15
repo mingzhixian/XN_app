32ef273b8731bb7ce3ae4a283570f77e4566157c
mingzhixian <mingzhixian123@gmail.com>
2023-06-15 09:48:29 +0800

    更新

---
+++ app/src/main/java/edu/swu/xn/app/LogIn.kt
@@ -0,0 +1,48 @@
+package edu.swu.xn.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.swu.xn.app.ui.theme.XN_appTheme
+
+class LogIn : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            XN_appTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    drawUI()
+                }
+            }
+        }
+    }
+
+    // 绘制UI
+    @Preview(showBackground = true)
+    @Composable
+    private fun drawUI(){
+
+    }
+
+    // 输入框
+    private fun setBackground(){
+        // 右上角图标
+        val icon= Icon(
+            imageVector =vectorResource()
+        )
+    }
+}
