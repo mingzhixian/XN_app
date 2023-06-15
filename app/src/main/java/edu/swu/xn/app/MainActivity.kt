c5771324db57c70f4781cbebf3e07fd8bbea86ed
mingzhixian <mingzhixian123@gmail.com>
2023-06-15 19:34:14 +0800

    更新

--- app/src/main/java/edu/swu/xn/app/MainActivity.kt
+++ app/src/main/java/edu/swu/xn/app/MainActivity.kt
@@ -3,7 +3,6 @@ package edu.swu.xn.app
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -44,196 +43,196 @@ import androidx.lifecycle.ViewModelProvider
 import edu.swu.xn.app.component.MyButton
 import edu.swu.xn.app.entity.Operation
 import edu.swu.xn.app.helper.AppData
-import edu.swu.xn.app.ui.theme.XN_appTheme
 
 lateinit var appData: AppData
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        appData = ViewModelProvider(this).get(AppData::class.java)
-        if (!appData.isInit) appData.init(this)
-        // 第一次使用
-        if (appData.settings.getBoolean("FirstUse", true)) firstUse()
-        // 检测登录
-
-        startActivity(Intent(
-            this,
-            LogIn::class.java
-        ))
-    }
-
-    @Composable
-    fun HomePage(
-        modifier: Modifier = Modifier,
-        name: String = "",
-        fontSize: Int = 14,
-        icon: Painter = painterResource(id = R.drawable.usericon),
-        iconSize: Dp = 48.dp,
-        searchOnClick: () -> Unit = {},
-        messageOnClick: () -> Unit = {},
-        operations: List<Operation> = listOf(
-            Operation("挂号", painterResource(id = R.drawable.register)) {
-
-            },
-            Operation("缴费", painterResource(id = R.drawable.pay)) {
-
-            },
-            Operation("报告单", painterResource(id = R.drawable.report)) {
-
-            },
-            Operation("订单", painterResource(id = R.drawable.order)) {
-
-            },
-            Operation("智能导诊", painterResource(id = R.drawable.intelligent_guidance)) {
-
-            },
-            Operation("待办", painterResource(id = R.drawable.pending)) {
-
-            }
-        )
-
-    ) {
-        Box(
-            modifier = modifier.background(Color.White)
-        )
-        {
-            Canvas(
-                modifier = Modifier.fillMaxSize()
-            ) {
-
-                scale(scaleX = 15f, scaleY = 10f) {
-                    drawCircle(
-                        Color.Blue,
-                        radius = 40.dp.toPx(),
-                        center = this.center + Offset(0f, -160f)
-                    )
-                }
-            }
-            Column(
-                modifier = Modifier
-            ) {
-                /* 顶部欢迎栏 */
-                Row(
-                    modifier = Modifier
-                        .background(Color.Transparent)
-                        .padding(top = 20.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    // 右侧
-                    Row(
-                        modifier = Modifier
-                            .background(Color.Transparent)
-                            .weight(0.5f)
-                            .padding(start = 20.dp)
-                    ) {
-                        Icon(
-                            painter = icon,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(iconSize)
-                                .clip(CircleShape),
-                            tint = Color.Unspecified
-                        )
-                        Spacer(modifier = Modifier.size(8.dp))
-                        Column {
-                            Text(
-                                text = "Hi $name,",
-                                fontSize = fontSize.sp,
-                                color = Color.White
-                            )
-                            Text(
-                                text = "欢迎回来！",
-                                fontSize = (fontSize - 2).sp,
-                                color = Color.LightGray
-                            )
-                        }
-                    }
-                    // 左侧
-                    Row(
-                        modifier = Modifier
-                            .background(Color.Transparent)
-                            .weight(0.5f),
-                        horizontalArrangement = Arrangement.End
-                    ) {
-                        IconButton(onClick = searchOnClick) {
-                            Icon(
-                                imageVector = Icons.Filled.Search,
-                                contentDescription = null,
-                                modifier = Modifier.size(20.dp),
-                                tint = Color.White
-                            )
-                        }
-                        IconButton(onClick = messageOnClick) {
-                            Icon(
-                                imageVector = Icons.Default.Email,
-                                contentDescription = null,
-                                modifier = Modifier.size(20.dp),
-                                tint = Color.White
-                            )
-                        }
-                    }
-                }
-                Spacer(modifier = Modifier.size(20.dp))
-                /* 病历 + 人 */
-                Row {
-
-                }
-                /* 卡片操作栏 */
-                Card(
-                    modifier = Modifier.padding(
-                        start = 20.dp,
-                        end = 20.dp
-                    ),
-                    colors = CardDefaults.cardColors(
-                        containerColor = Color.White,
-                    ),
-                    elevation = CardDefaults.cardElevation(
-                        defaultElevation = 4.dp
-                    )
-                ) {
-                    LazyVerticalGrid(
-                        columns = GridCells.Adaptive(minSize = 128.dp),
-                        contentPadding = PaddingValues(20.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    )
-                    {
-                        items(operations.size) {
-                            MyButton(
-                                modifier = Modifier
-                                    .height(80.dp)
-                                    .padding(4.dp),
-                                text = operations[it].text,
-                                icon = operations[it].icon,
-                                onClick = operations[it].onClick
-                            ).show()
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    @Preview
-    @Composable
-    fun HomePagePreview() {
-        HomePage(
-            name = "小老弟"
-        )
-    }
-
-
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    appData = ViewModelProvider(this).get(AppData(this)::class.java)
     // 第一次使用
-    private fun firstUse() {
-        // 通知注册
-        appData.notificationHelper.registerChannel()
-        // 权限申请
-        appData.notificationHelper.requireNotificationPermission()
-        // 保存
-        appData.settings.edit().apply {
-            putBoolean("FirstUse", false)
-            apply()
+    if (appData.settings.getBoolean("FirstUse", true)) firstUse()
+    // 检测登录
+
+    startActivity(
+      Intent(
+        this,
+        LogIn::class.java
+      )
+    )
+  }
+
+  @Composable
+  fun HomePage(
+    modifier: Modifier = Modifier,
+    name: String = "",
+    fontSize: Int = 14,
+    icon: Painter = painterResource(id = R.drawable.usericon),
+    iconSize: Dp = 48.dp,
+    searchOnClick: () -> Unit = {},
+    messageOnClick: () -> Unit = {},
+    operations: List<Operation> = listOf(
+      Operation("挂号", painterResource(id = R.drawable.register)) {
+
+      },
+      Operation("缴费", painterResource(id = R.drawable.pay)) {
+
+      },
+      Operation("报告单", painterResource(id = R.drawable.report)) {
+
+      },
+      Operation("订单", painterResource(id = R.drawable.order)) {
+
+      },
+      Operation("智能导诊", painterResource(id = R.drawable.intelligent_guidance)) {
+
+      },
+      Operation("待办", painterResource(id = R.drawable.pending)) {
+
+      }
+    )
+
+  ) {
+    Box(
+      modifier = modifier.background(Color.White)
+    )
+    {
+      Canvas(
+        modifier = Modifier.fillMaxSize()
+      ) {
+
+        scale(scaleX = 15f, scaleY = 10f) {
+          drawCircle(
+            Color.Blue,
+            radius = 40.dp.toPx(),
+            center = this.center + Offset(0f, -160f)
+          )
         }
+      }
+      Column(
+        modifier = Modifier
+      ) {
+        /* 顶部欢迎栏 */
+        Row(
+          modifier = Modifier
+            .background(Color.Transparent)
+            .padding(top = 20.dp),
+          horizontalArrangement = Arrangement.SpaceBetween,
+          verticalAlignment = Alignment.CenterVertically
+        ) {
+          // 右侧
+          Row(
+            modifier = Modifier
+              .background(Color.Transparent)
+              .weight(0.5f)
+              .padding(start = 20.dp)
+          ) {
+            Icon(
+              painter = icon,
+              contentDescription = null,
+              modifier = Modifier
+                .size(iconSize)
+                .clip(CircleShape),
+              tint = Color.Unspecified
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Column {
+              Text(
+                text = "Hi $name,",
+                fontSize = fontSize.sp,
+                color = Color.White
+              )
+              Text(
+                text = "欢迎回来！",
+                fontSize = (fontSize - 2).sp,
+                color = Color.LightGray
+              )
+            }
+          }
+          // 左侧
+          Row(
+            modifier = Modifier
+              .background(Color.Transparent)
+              .weight(0.5f),
+            horizontalArrangement = Arrangement.End
+          ) {
+            IconButton(onClick = searchOnClick) {
+              Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = null,
+                modifier = Modifier.size(20.dp),
+                tint = Color.White
+              )
+            }
+            IconButton(onClick = messageOnClick) {
+              Icon(
+                imageVector = Icons.Default.Email,
+                contentDescription = null,
+                modifier = Modifier.size(20.dp),
+                tint = Color.White
+              )
+            }
+          }
+        }
+        Spacer(modifier = Modifier.size(20.dp))
+        /* 病历 + 人 */
+        Row {
+
+        }
+        /* 卡片操作栏 */
+        Card(
+          modifier = Modifier.padding(
+            start = 20.dp,
+            end = 20.dp
+          ),
+          colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+          ),
+          elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+          )
+        ) {
+          LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 128.dp),
+            contentPadding = PaddingValues(20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+          )
+          {
+            items(operations.size) {
+              MyButton(
+                modifier = Modifier
+                  .height(80.dp)
+                  .padding(4.dp),
+                text = operations[it].text,
+                icon = operations[it].icon,
+                onClick = operations[it].onClick
+              ).show()
+            }
+          }
+        }
+      }
     }
+  }
+
+  @Preview
+  @Composable
+  fun HomePagePreview() {
+    HomePage(
+      name = "小老弟"
+    )
+  }
+
+
+  // 第一次使用
+  private fun firstUse() {
+    // 通知注册
+    appData.notificationHelper.registerChannel()
+    // 权限申请
+    appData.notificationHelper.requireNotificationPermission()
+    // 保存
+    appData.settings.edit().apply {
+      putBoolean("FirstUse", false)
+      apply()
+    }
+  }
 }
