7030994016b7a8470e946d4f493b550e600df33a
OoShawnoO <840831038@qq.com>
2023-06-15 16:03:54 +0800

    [+] 主页

--- app/src/main/java/edu/swu/xn/app/component/OperateItem.kt
+++ app/src/main/java/edu/swu/xn/app/component/OperateItem.kt
@@ -3,17 +3,17 @@ package edu.swu.xn.app.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
@@ -26,23 +26,28 @@ import androidx.compose.ui.unit.dp
 fun OperateItem(
     modifier: Modifier = Modifier,
     text: String = "",
-    textColor: Color = Color.White,
+    textColor: Color = Color.LightGray,
+    onClick: () -> Unit = {},
     icon: ImageVector = Icons.Filled.Done,
     iconSize: Dp = 80.dp
 ) {
     Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = modifier.border(width = 0.2f.dp, color = Color.DarkGray),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Image(
-            imageVector = icon,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(iconSize)
-                .clip(CircleShape)
-                .background(Color.White)
-        )
+        OutlinedButton(
+            onClick = onClick,
+            border = null,
+        ) {
+            Image(
+                imageVector = icon,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(iconSize)
+                    .background(Color.Transparent)
+            )
+        }
         Spacer(
             modifier = Modifier.size(4.dp)
         )
