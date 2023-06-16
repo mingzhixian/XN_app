e34de0f0a75a6c1a96fd513466fc0e0e96409227
mingzhixian <mingzhixian123@gmail.com>
2023-06-16 16:11:48 +0800

    更新

--- app/src/main/java/edu/swu/xn/app/helper/NetHelper.kt
+++ app/src/main/java/edu/swu/xn/app/helper/NetHelper.kt
@@ -2,6 +2,7 @@ package edu.swu.xn.app.helper
 
 import android.content.ContentValues
 import android.util.Log
+import android.widget.Toast
 import edu.swu.xn.app.appData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -17,17 +18,22 @@ import org.json.JSONObject
 
 
 class NetHelper {
-  val okhttpClient = OkHttpClient()
+  private val okhttpClient = OkHttpClient()
 
   // 网络获取JSON
   fun get(url: String, handle: (json: JSONObject) -> Unit) {
     appData.mainScope.launch {
       withContext(Dispatchers.IO) {
-        val request: Request = Request.Builder()
-          .url(url)
-          .build()
-        okhttpClient.newCall(request).execute().use { response ->
-          handle(JSONObject(response.body!!.string()))
+        val request = Request.Builder().url(url)
+        if (appData.hashID != "") {
+          request.addHeader("token", appData.hashID)
+        }
+        try {
+          okhttpClient.newCall(request.build()).execute().use { response ->
+            handle(JSONObject(response.body!!.string()))
+          }
+        } catch (_: Exception) {
+          Toast.makeText(appData.main, "网络错误", Toast.LENGTH_SHORT).show()
         }
       }
     }
@@ -36,14 +42,18 @@ class NetHelper {
   fun get(url: String, value: JSONObject, handle: (json: JSONObject) -> Unit) {
     appData.mainScope.launch {
       withContext(Dispatchers.IO) {
-        val requestBody = value.toString()
-          .toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
-        val request = Request.Builder()
-          .url(url)
-          .post(requestBody)
-          .build()
-        okhttpClient.newCall(request).execute().use { response ->
-          handle(JSONObject(response.body!!.string()))
+        val requestBody =
+          value.toString().toRequestBody("application/json; charset=utf-8".toMediaTypeOrNull())
+        val request = Request.Builder().url(url).post(requestBody)
+        if (appData.hashID != "") {
+          request.addHeader("token", appData.hashID)
+        }
+        try {
+          okhttpClient.newCall(request.build()).execute().use { response ->
+            handle(JSONObject(response.body!!.string()))
+          }
+        } catch (_: Exception) {
+          Toast.makeText(appData.main, "网络错误", Toast.LENGTH_SHORT).show()
         }
       }
     }
